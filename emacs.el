f8c710b8789c45e1ce57e607115fd7c5597453e0
Issei Horie <horie7188@gmail.com>
2017-09-18 16:56:47 +0900

    Update emacs.el

--- emacs.el
+++ emacs.el
@@ -5,3 +5,8 @@
 ;; Japanese settings
 (set-language-environment 'Japanese)
 (prefer-coding-system 'utf-8)
+
+; neotree
+(add-to-list 'load-path "~/.emacs.d/plugin/neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
