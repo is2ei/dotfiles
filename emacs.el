1ef3cf2192d3fb86e89f074152ba383a6e210058
Issei Horie <horie7188@gmail.com>
2017-11-02 00:02:23 +0900

    Updated emacs.el

--- emacs.el
+++ emacs.el
@@ -16,3 +16,8 @@
 (add-to-list 'load-path "~/.emacs.d/plugin/rust-mode")
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+; typemacs
+(add-to-list 'load-path "~/.emacs.d/plugin/typemacs")
+(require 'typemacs)
+(global-set-key [f9] 'typemacs-start)
